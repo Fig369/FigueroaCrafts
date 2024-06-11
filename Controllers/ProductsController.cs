81a2f7834f7efdd20871b0f169b2f8b017bcc9e5
Fig <72757855+Fig369@users.noreply.github.com>
2024-06-11 02:01:43 -0400

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using FigueroaCrafts.WebSite.Models;
+using FigueroaCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FigueroaCrafts.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) =>
+            ProductService = productService;
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            if (request?.ProductId == null)
+                return BadRequest();
+
+            ProductService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string? ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
